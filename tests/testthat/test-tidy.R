76773c2d983ab76c2e2cc37b54a30dee3818a908
nmercadeb <nmercade147@gmail.com>
2024-04-20 18:50:33 +0100

    checks

--- tests/testthat/test-tidy.R
+++ tests/testthat/test-tidy.R
@@ -3,7 +3,7 @@ test_that("tidy", {
 
   expect_no_error(res0 <- tidy(x = mocksum, pivotEstimatesBy = "estimate_name"))
   expect_true(nrow(res0 |> dplyr::filter(.data$variable_name == "settings")) == 0)
-  expect_true(all(c("cohort_name", "age_group", "sex", "mock_default", "count",
+  expect_true(all(c("cohort_name", "age_group", "sex", "count",
                     "mean", "sd", "percentage") %in% colnames(res0)))
   expect_true(class(res0$percentage) == "numeric")
   expect_true(class(res0$mean) == "numeric")
@@ -12,11 +12,11 @@ test_that("tidy", {
   expect_no_error(res1 <- tidy(mocksum, splitGroup = FALSE, splitAdditional = FALSE,
                                splitStrata = FALSE, pivotEstimatesBy = c("variable_name", "variable_level", "estimate_name")))
   expect_true(all(c("group_name", "group_level", "strata_name", "strata_level",
-                    "additional_name", "additional_level", "mock_default") %in%
+                    "additional_name", "additional_level") %in%
                     colnames(res1)))
   expect_true(all(c("number subjects_NA_count", "age_NA_mean", "age_NA_sd", "Medications_Amoxiciline_count",
                     "Medications_Amoxiciline_percentage", "Medications_Ibuprofen_count",
-                    "Medications_Ibuprofen_percentage", "mock_default") %in% colnames(res1)))
+                    "Medications_Ibuprofen_percentage") %in% colnames(res1)))
 
   expect_no_error(res2 <- tidy(mocksum,
                                splitGroup = FALSE,
@@ -24,7 +24,6 @@ test_that("tidy", {
                                addSettings = FALSE,
                                pivotEstimatesBy = c("variable_name", "estimate_name"),
                                nameStyle = "{estimate_name}__{variable_name}"))
-  expect_false("mock_default" %in% colnames(res2))
   expect_false("logic__settings" %in% colnames(res2))
   expect_true(all(c("count__number subjects", "mean__age", "sd__age",
                     "count__Medications", "percentage__Medications") %in% colnames(res2)))
@@ -35,7 +34,7 @@ test_that("tidy", {
   expect_no_error(res3 <- tidy(mocksum, splitGroup = FALSE, splitAdditional = FALSE, splitStrata = FALSE,
                                 pivotEstimatesBy = NULL))
   expect_true(all(colnames(res3) %in% c(
-    colnames(mocksum), "mock_default", "result_type", "package_name", "package_version"
+    colnames(mocksum), "result_type", "package_name", "package_version"
   )))
 
   # 2 id's:
