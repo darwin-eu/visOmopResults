d82d1cc7c93297d7d0756fcdc80a4498c156debd
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2024-04-29 12:36:05 +0100

    format estimate name

--- R/formatEstimateName.R
+++ R/formatEstimateName.R
@@ -70,15 +70,12 @@ formatEstimateNameInternal <- function(result, format, keepNotFormatted, useForm
   ]
 
   # start formatting
-  isSummarisedResult <- inherits(result, "summarised_result")
-  if (isSummarisedResult) {
-    settings <- omopgenerics::settings(result)
-  }
+  settings <- attr(result, "settings")
+  classes <- class(result)
+
   result <- result |>
     dplyr::mutate("formatted" = FALSE, "id" = dplyr::row_number()) |>
-    dplyr::group_by(dplyr::across(dplyr::all_of(cols))) |>
-    dplyr::mutate(group_id = min(.data$id)) |>
-    dplyr::ungroup()
+    dplyr::mutate(group_id = min(.data$id), .by = dplyr::all_of(cols))
 
   resultF <- NULL
   for (k in seq_along(format)) {
@@ -93,11 +90,9 @@ formatEstimateNameInternal <- function(result, format, keepNotFormatted, useForm
       res <- result |>
         dplyr::filter(!.data$formatted) |>
         dplyr::filter(.data$estimate_name %in% .env$keysK) |>
-        dplyr::group_by(dplyr::across(dplyr::all_of(cols))) |>
-        dplyr::filter(dplyr::n() == .env$len) |>
-          dplyr::mutate("id" = min(.data$id))
+        dplyr::filter(dplyr::n() == .env$len, .by = dplyr::all_of(cols)) |>
+        dplyr::mutate("id" = min(.data$id), .by = dplyr::all_of(cols))
       resF <- res |>
-        dplyr::ungroup() |>
         dplyr::select(-"estimate_type") |>
         tidyr::pivot_wider(
           names_from = "estimate_name", values_from = "estimate_value"
@@ -126,14 +121,15 @@ formatEstimateNameInternal <- function(result, format, keepNotFormatted, useForm
   #useFormatOrder
   if (useFormatOrder) {
     new_order <- dplyr::tibble(estimate_name = nms, format_id = 1:length(nms)) |>
-      dplyr::union_all(result |>
-                         dplyr::select("estimate_name") |>
-                         dplyr::distinct() |>
-                         dplyr::filter(!.data$estimate_name %in% nms) |>
-                         dplyr::mutate(format_id = length(format) + dplyr::row_number()))
+      dplyr::union_all(
+        result |>
+          dplyr::select("estimate_name") |>
+          dplyr::distinct() |>
+          dplyr::filter(!.data$estimate_name %in% nms) |>
+          dplyr::mutate(format_id = length(format) + dplyr::row_number())
+      )
     result <- result |>
-      dplyr::left_join(new_order,
-                       by = "estimate_name")
+      dplyr::left_join(new_order, by = "estimate_name")
     result <- result[order(result$group_id, result$format_id, decreasing = FALSE),] |>
       dplyr::select(-c("id", "group_id", "format_id"))
   } else {
@@ -145,13 +141,9 @@ formatEstimateNameInternal <- function(result, format, keepNotFormatted, useForm
   if (!keepNotFormatted) {
     result <- result |> dplyr::filter(.data$formatted)
   }
-  # result
-  result <- result |>
-    dplyr::select(-"formatted")
-  if (isSummarisedResult) {
-    result <- result |>
-      omopgenerics::newSummarisedResult(settings = settings)
-  }
+
+  result <- result |> dplyr::select(-"formatted")
+
   return(result)
 }
 getFormatNum <- function(format, keys) {
--- tests/testthat/test-formatEstimateName.R
+++ tests/testthat/test-formatEstimateName.R
@@ -5,7 +5,7 @@ test_that("formatEstimateName", {
   # input 1 ----
   result_output <-  formatEstimateName(result,
                                        estimateNameFormat = c("N (%)" = "<count> (<percentage>%)",
-                                                  "N" = "<count>"),
+                                                              "N" = "<count>"),
                                        keepNotFormatted = TRUE)
   # check count as "N"
   expect_identical(unique(result_output$estimate_name[result_output$variable_name == "number subjects"]),
@@ -34,11 +34,14 @@ test_that("formatEstimateName", {
                      " (",
                      estimates_in$estimate_value[estimates_in$variable_name == "Medications" &  estimates_in$estimate_name == "percentage"],
                      "%)"))
+  # attributes mantained:
+  expect_true(nrow(omopgenerics::settings(result_output)) == 1)
+  expect_true(inherits(result_output, "summarised_result"))
 
   # input 2 ----
   result_output <-  formatEstimateName(result,
                                        estimateNameFormat = c("<mean> (<sd>)",
-                                                  "N%" = "<count> (<percentage> %)"),
+                                                              "N%" = "<count> (<percentage> %)"),
                                        keepNotFormatted = FALSE)
   # Check not keep formatted
   expect_true(result_output |>
@@ -130,9 +133,9 @@ test_that("formatEstimateName", {
                                     keepNotFormatted = TRUE),
                  "has not been formatted.")
   expect_message(formatEstimateName(result,
-                     estimateNameFormat = c("N" = "count",
-                                            "N (%)" = "<count> (<percentage>%)"),
-                     keepNotFormatted = FALSE),
+                                    estimateNameFormat = c("N" = "count",
+                                                           "N (%)" = "<count> (<percentage>%)"),
+                                    keepNotFormatted = FALSE),
                  "does not contain an estimate name indicated by <...>")
   expect_error(formatEstimateName(result,
                                   estimateNameFormat = NA,
@@ -260,11 +263,11 @@ test_that("formatEstimateName, useFormatOrder", {
                                        useFormatOrder = TRUE)
 
   expect_false(any(which(result_output$estimate_name %in% "range") <
-                    which(result_output$estimate_name %in% "mean")))
+                     which(result_output$estimate_name %in% "mean")))
   expect_false(any(which(result_output$estimate_name %in% "number subjets") <
-                    which(result_output$estimate_name %in% "mean")))
+                     which(result_output$estimate_name %in% "mean")))
   expect_false(any(which(result_output$estimate_name %in% "number subjets") <
-                    which(result_output$estimate_name %in% "range")))
+                     which(result_output$estimate_name %in% "range")))
 
 })
 
@@ -284,4 +287,20 @@ test_that("empty format",{
     useFormatOrder = TRUE)
   )
   expect_identical(res1, result)
+})
+
+
+test_that("not a summarised result",{
+  result <- dplyr::tibble(
+    variable = "variable",
+    estimate_type = c("integer", "numeric"),
+    estimate_name = c("count", "percentage"),
+    estimate_value = c("100", "50.1")
+  )
+  res0 <- formatEstimateName(
+    result,
+    estimateNameFormat = c("N (%)" = "<count> (<percentage>%)")
+  )
+  expect_true(nrow(res0) == 1)
+  expect_equal(class(result), class(res0))
 })
