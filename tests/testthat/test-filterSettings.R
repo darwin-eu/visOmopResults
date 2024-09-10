3d777d36d0560b9ce3f960fd2d0e1e38a0da8e80
catalamarti <marticatalasabate@gmail.com>
2024-09-10 22:22:17 +0100

    failing test

--- tests/testthat/test-filterSettings.R
+++ tests/testthat/test-filterSettings.R
@@ -5,8 +5,16 @@ test_that("filterSettings", {
                     filterSettings(result_type == "omock")
   )
 
-
   expect_identical(result, result1)
 
+  result <- mockSummarisedResult()
+  expect_identical(
+    result,
+    result |> filterSettings(result_type == "mock_summarised_result")
+  )
+  expect_warning(
+    result0 <- result |> filterSettings(variable_does_not_exist == TRUE)
+  )
+  expect_true(nrow(result0) == 0)
 
 })
